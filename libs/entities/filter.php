13cf2d8e0a250d9f9d0209c732526c5b12e5b78a
Hugo Lapointe Di Giacomo <hlapointe@dutailier.com>
2013-05-31 09:35:19 -0400

    Ajout de filter.php

---
+++ libs/entities/filter.php
@@ -0,0 +1,39 @@
+<?php
+
+include_once(ROOT . 'libs/entity.php');
+include_once(ROOT . 'libs/repositories/products.php');
+
+class Filter extends Entity
+{
+    private $name;
+    private $type;
+
+    function __construct($name, $type)
+    {
+        $this->name = $name;
+        $this->type = $type;
+    }
+
+    public function getArray()
+    {
+        return array(
+            'id' => $this->getId(),
+            'name' => $this->getName()
+        );
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    public function getProducts()
+    {
+        return Products::FilterByFilterId($this->getId());
+    }
+}
